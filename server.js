d5da481d5450d32ac5e8ca4e38b2de69bca0084e
Riza Pranata <corodotc@gmail.com>
2019-10-29 11:09:06 +0700

    config body parser

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//memanggil library body parser
+const bodyParser = require("body-parser");
+//confog body parser
+app.use(bodyParser.urlencoded({ extended: true })); //menangkap type request dalam bentuk form urllencode
+//menangkap url dalam bentuk json
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Hello World'))
 
+app.post('/hello', function (req, res) {
+    const respon = {
+        ststusCode: 200,
+        error: "",
+        message: "Hello Json"
+    }
+    res.json(respon);
+})
 
 app.listen(port, () => console.log(`Express app listening on port ${port}!`))
